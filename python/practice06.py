0135992aac542bb078eb5f131e0995b71c08f65b
lzsy1992 <365042575@qq.com>
2018-10-11 00:15:41 +0800

    compelete

--- python/practice06.py
+++ python/practice06.py
@@ -1,9 +1,17 @@
 import mysql.connector
 
 conn = mysql.connector.connect(user="root", password="rong1987", database="test")
+'''
 cursor = conn.cursor()
-cursor.execute('create table user (id varchar(20) primary key, name varchar(20))')
-cursor.execute('insert into user (id, name) values (%s, %s)', ['12', 'Jack'])
+cursor.execute('create table users (id varchar(20) primary key, name varchar(20))')
+cursor.execute('insert into users (id, name) values (%s, %s)', ['12', 'Jack'])
 print('rowcount=',cursor.rowcount)
 conn.commit()
+conn.close()
+'''
+cursor = conn.cursor()
+cursor.execute('select * from users where id = %s', ('12',))
+value = cursor.fetchall()
+print(value)
+cursor.close()
 conn.close()
