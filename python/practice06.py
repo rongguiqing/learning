0309f03ff41504997736e34f945ef54b72e4126e
rongguiqing <ronggq1987@126.com>
2018-10-26 13:07:41 +0800

    add

--- python/practice06.py
+++ python/practice06.py
@@ -2,7 +2,7 @@ import mysql.connector
 
 conn = mysql.connector.connect(user="root", password="rong1987", database="test")
 cursor = conn.cursor()
-cursor.execute('create table user (id varchar(20) primary key, name varchar(20))')
+cursor.execute('create table user if(id varchar(20) primary key, name varchar(20))')
 cursor.execute('insert into user (id, name) values (%s, %s)', ['12', 'Jack'])
 print('rowcount=',cursor.rowcount)
 conn.commit()
